08c3ed6c4d17eedd16fedfac1e74ff211b1bcab3
Sofia Ivanova <sofia.iivanova@gmail.com>
2025-03-10 11:14:14 +0200

    ,

---
+++ zad2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node<int> head = new Node<int>(10);
+            head.Next = new Node<int>(20);
+            head.Next.Next = new Node<int>(30);
+            head.Next.Next.Next = new Node<int>(40);
+            head.Next.Next.Next.Next = new Node<int>(50);
+            head.Next.Next.Next.Next.Next = new Node<int>(60);
+            Console.WriteLine("The middle is: " + Reverser<int>.GetMiddle(head));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ zad2/Reverser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zad2
+{
+    internal class Reverser<T>
+    {
+        public static T GetMiddle(Node<T> head)
+        {
+            Node<T> slow_ptr = head;
+            Node<T> fast_ptr = head;
+            while (fast_ptr != null && fast_ptr.Next != null)
+            {
+                fast_ptr = fast_ptr.Next.Next;
+                slow_ptr = slow_ptr.Next;
+            }
+            return slow_ptr.Data;
+        }
+    }
+}
